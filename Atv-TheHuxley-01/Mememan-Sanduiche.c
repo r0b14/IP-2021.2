6ee2b12d8167527a2b846dbc0e187ba84da2f62c
Robson Thiago <rtth141200@gmail.com>
2022-02-28 17:32:14 -0300

    comentado o programa
    Programa comentado para um melhor entendimento.

--- Atv-TheHuxley-01/Mememan-Sanduiche.c
+++ Atv-TheHuxley-01/Mememan-Sanduiche.c
@@ -1,36 +1,48 @@
+/*Meme Man Sanduiche*/
 #include <stdio.h>
 
 int main()
 {
-    char i0, i1, i2, i3, i4, i5, i6;
-    scanf("%c%c%c%c%c%c%c", &i0, &i1, &i2, &i3, &i4, &i5, &i6);
-    int x = 0;
-    if (i5 != 'P') {
-        printf("Invalido\n...Mas pelo menos ganhou um chute\n");
-    } else {
-        if (((i0 == '.') || (i0 == ':') || (i6 == '.') || (i6 == ':')) && (i0 == i6)) {
-          x++;
-        }
-        if ((i1 == 'b') || (i1 == 'f') || (i1 == 'p') || (i1 == ' ')) {
-          x++;
-        }
-        if ((i2 == 'A') || (i2 == 'T') || (i2 == 'C')  ||(i2 == 'R') || (i2 == ' ')) {
-          x++;
-        }
-        if ((i3 == 'A') || (i3 == 'T') || (i3 == 'C') || (i3 == 'R') || (i3 == ' ')) {
-          x++;
-        }
-        if ((i4 == 'A') || (i4 == 'T') || (i4 == 'C') || (i4 == 'R') || (i4 == ' ')) {
-          x++;
-        }
-        if (i5 == 'P') {
-          x++;
-        }
-        if (x == 6) {
-          printf("Valido\n");
-        } else {
-          printf("Invalido\n");
-        }
+
+  char i0, i1, i2, i3, i4, i5, i6;
+  scanf("%c%c%c%c%c%c%c", &i0, &i1, &i2, &i3, &i4, &i5, &i6);
+  int x = 0; //Contador
+
+  if (i5 != 'P') { // Eh obrigatorio o uso de molho picante no ultimo ingrediente (T4)
+    printf("Invalido\n...Mas pelo menos ganhou um chute\n");
+  }
+  else {
+    // O uso de pao - T1
+    if (((i0 == '.') || (i0 == ':') || (i6 == '.') || (i6 == ':')) && (i0 == i6)) {
+      x++; 
     }
-    return 0;
+    //Uso da carne - T2
+    if ((i1 == 'b') || (i1 == 'f') || (i1 == 'p') || (i1 == ' ')) {
+      x++;
+    }
+    //Uso do extra - T3
+    if ((i2 == 'A') || (i2 == 'T') || (i2 == 'C')  ||(i2 == 'R') || (i2 == ' ')) {
+      x++;
+    }
+    //Uso do extra - T3
+    if ((i3 == 'A') || (i3 == 'T') || (i3 == 'C') || (i3 == 'R') || (i3 == ' ')) {
+      x++;
+    }
+    //Uso do extra - T3
+    if ((i4 == 'A') || (i4 == 'T') || (i4 == 'C') || (i4 == 'R') || (i4 == ' ')) {
+      x++;
+    }
+    //Uso do Molho - T4 casca de banana
+    if (i5 == 'P') {
+      x++;
+    }
+    //Verificando contador
+    if (x == 6) {
+      printf("Valido\n");
+    }
+    else {
+      printf("Invalido\n");
+    }
+  }
+  return 0;
 }
