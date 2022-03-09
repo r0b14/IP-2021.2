b91d17ec20c2ee3efe36fba7da911a84a3f66248
Robson Thiago <56169778+r0b14@users.noreply.github.com>
2022-03-09 18:56:05 -0300

    Create ressaca.c

---
+++ Atv-TheHuxley-04/ressaca.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define SIZE 20
+int main() {
+
+    int i, num;
+    scanf("%d", &num);
+    char funcion[SIZE];
+    for(i = 0; i < num; i++){
+        scanf("");
+    }
+    return 0;
+
+}
