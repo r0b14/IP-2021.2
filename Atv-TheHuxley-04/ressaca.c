1b7dc91b6905617e2700dc6a51e79a8a8048e31c
Robson Thiago <rtth141200@gmail.com>
2022-03-10 15:39:56 -0300

    atualizada a questao
    tentativa de acerto.

--- Atv-TheHuxley-04/ressaca.c
+++ Atv-TheHuxley-04/ressaca.c
@@ -1,14 +1,49 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 
-#define SIZE 20
 int main() {
 
-    int i, num;
+    int num= 0, i, j; 
+    char x, y;
     scanf("%d", &num);
-    char funcion[SIZE];
-    for(i = 0; i < num; i++){
-        scanf("");
-    }
-    return 0;
 
+    char comando[num + 1][10];
+    for (i= 0; i< num + 1; i++){
+        gets(comando[i]);
+    }
+
+    //Procurando as plavras Chaves
+    for (i = 0; i < num + 1; i++){
+        if (strncmp(comando[i], "mov", 3) == 0){
+            x = comando[i][4];
+            y = comando[i][7];
+            strcpy(comando[i], " ");
+            sprintf(comando[i], "%c = %c", x, y);
+            printf("%s\n", comando[i]);
+        }
+        if (strncmp(comando[i], "add", 3) == 0){
+            x = comando[i][4];
+            y = comando[i][7];
+            strcpy(comando[i], " ");
+            sprintf(comando[i], "%c += %c", x, y);
+            printf("%s\n", comando[i]);
+        }
+        if (strncmp(comando[i], "sub", 3) == 0){
+            x = comando[i][4];
+            y = comando[i][7];
+            strcpy(comando[i], " ");
+            sprintf(comando[i], "%c -= %c", x, y);
+            printf("%s\n", comando[i]);
+        }
+        if (strncmp(comando[i], "and", 3) == 0){
+            x = comando[i][4];
+            y = comando[i][7];
+            strcpy(comando[i], " ");
+            sprintf(comando[i], "%c = %c & %c", x, x, y);
+            printf("%s\n", comando[i]);
+        }
+    }
+
+    return 0;
 }
