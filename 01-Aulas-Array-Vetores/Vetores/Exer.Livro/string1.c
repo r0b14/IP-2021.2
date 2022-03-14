ca5ff4fd26fd94953e842ff31331a8b3bb288c03
Robson Thiago <rtth141200@gmail.com>
2022-03-14 12:57:38 -0300

    renomeado arquivo para melhor entedimento

--- 01-Aulas-Array-Vetores/Vetores/Exer.Livro/string1.c
+++ 01-Aulas-Array-Vetores/Vetores/Exer.Livro/string1.c
@@ -7,16 +7,16 @@
 #include <stdlib.h>
 #include <string.h>
 
-    /* função main inicia a execução do programa */
+/* função main inicia a execução do programa */
 int main( void )
 {
     char string1[20]; /* reserva 20 caracteres */
-    char string2[] = "string literal"; /* reserva 15 caracteres */
+    char string2[] = "string literal"; /* reserva 14 + '\0' caracteres mediante a string inserida */
     int i; /* contador */
 
     /* lê substring do usuário para array string1 */
     printf("Digite uma string: ");
-    scanf("%s", string1 ); /* entrada terminada com espaço em branco */
+    scanf("%s", string1 ); /* entrada terminada com espaço em branco ou enter*/
 
     /* mostra strings */
     printf( "string1 é: %s\nstring2 is: %s\n"
@@ -24,11 +24,11 @@ int main( void )
     string1, string2 );
 
     /* mostra caracteres até o caractere nulo ser alcançado */
-    for ( i = 0; string1[ i ] != '\0'; i++ ) {
-    printf( "%c ", string1[ i ] );
+    for ( i = 0; string1[ i ] != '\0'; i++ ) { // Ate chegar ao caracter nulo '/0'
+    printf( "%c ", string1[ i ] ); /* inserindo espacos entre os caracteres */
     } /* fim do for */
 
-    printf( "\n");
+    printf("\n");
 
     return 0; /* indica conclusão bem-sucedida */
 } /* fim do main */
