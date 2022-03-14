0a259e69edb51223c6d56094f715f962856949e2
Robson Thiago <rtth141200@gmail.com>
2022-03-14 14:42:10 -0300

    2 parte do programa.

--- 01-Aulas-Array-Vetores/Vetores/Monitoria03-25-02/Exercicio01-array.c
+++ 01-Aulas-Array-Vetores/Vetores/Monitoria03-25-02/Exercicio01-array.c
@@ -9,16 +9,48 @@
 int main()
 {
     int quantSequencia, tamSequencia;
-    int index;
+    int maiorSequencia = 0, atualSequencia = 0, indexFinal = 0;
+    int index, i;
     scanf("%d", &quantSequencia); // Recebendo os valores do usuario
 
     while (quantSequencia > 0)
     {
         scanf("%d", &tamSequencia);
         int sequencia[tamSequencia] = { 0 };
-        for(index = 0; index < tamSequencia; index++)
+        for (index = 0; index < tamSequencia; index++)
         {
             scanf("%d", &sequencia[index]);
+        }
+        for (i = 1; i < tamSequencia; i++) //Percorrendo todo o vetor sequencia.
+        {
+            if (sequencia[i] < sequencia[i+1]) // Verificando se o antecessor é menor que o seu sucessor
+            {
+                atualSequencia++;
+            }
+            else
+            {
+                if(atualSequencia > maiorSequencia)
+                {
+                    maiorSequencia = atualSequencia;
+                    indexFinal = i - 1;
+                }
+                atualSequencia = 0;
+            }
+        }
+
+        if(atualSequencia > maiorSequencia)
+        {
+            maiorSequencia = atualSequencia;
+            indexFinal = tamSequencia - 1;
+        }
+
+        printf("%d\n", maiorSequencia);
+        if (maiorSequencia > 0)
+        {
+            for (i = indexFinal - maiorSequencia; i <= indexFinal ;i++)
+            {
+                printf("%d");
+            }
         }
         // Array sequencia totalmente preenchido
         --quantSequencia;
