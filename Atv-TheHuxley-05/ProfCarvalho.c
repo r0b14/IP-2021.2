8b7288b9c231107d20075312d1e3dbae66746f0a
Robson Thiago <rtth141200@gmail.com>
2022-03-18 17:44:06 -0300

    Tentando vencer a questao do prof

--- Atv-TheHuxley-05/ProfCarvalho.c
+++ Atv-TheHuxley-05/ProfCarvalho.c
@@ -2,9 +2,10 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define SIZE 20
+#define SIZE 21
+
 typedef struct {
-   char nome[SIZE];
+   char nome[SIZE - 1];
    char tipo[3];
    int ataque;
    int defesa;
@@ -12,24 +13,72 @@ typedef struct {
 
 int main() {
 
-   int quantPokemon, i, verif;
+   int quantPokemon, i, verif = 1;
+   char verfstatusf, pokemon1, pokemon2;
+   int defesapokemon1 = 0, ataquepokemon1 = 0, defesapokemon2 = 0, ataquepokemon2 = 0;
+   scanf("%d", &quantPokemon); // Recebendo a quantidade de pokemons
+
    Pokemon pokemons[quantPokemon];
-
-   scanf("%d", &quantPokemon);
-   while (verif != 0 )
-   {
-      for (i = 0 ; i < quantPokemon ; i++) {
-         scanf(" %s", pokemons[i].nome);
-         scanf(" %s", pokemons[i].tipo);
-         scanf("%d", pokemons[i].ataque);
-         scanf("%d", pokemons[i].defesa);
-      }
-      printf("luta\n");
-      scanf(" %s vs %s", );
+   for (i = 0 ; i < quantPokemon ; i++) {
+      scanf(" %s", pokemons[i].nome);
+      scanf(" %s", pokemons[i].tipo);
+      scanf("%d", &pokemons[i].ataque);
+      scanf("%d", &pokemons[i].defesa);
    }
-   scanf("%d", &verif);
+   /**
+    * Parte 2 -> List ou luta
+    * ## Luta ##
+    */
+   while ( verif != 0 && verif > 0) {
+      scanf(" %s", verfstatusf); // Verificando se e listagem ou luta
+      // LUTA
+      if ( strcmp(verfstatusf, "luta") == 0 ) {
+         scanf(" %s vs %s", pokemon1, pokemon2);
+         /// Varrendo o vetor para poder extrair os valores dos ataque e defesa
+         for ( i = 0 ; i < quantPokemon ; i++ ) {
+            // Encontrando o pokemon1 para poder pegar os valores dos poderes
+            if ( strcmp(pokemon1, pokemons[i].nome) == 0 ) { // Encontrodou dentro do vetor o nome do pokemon1
+               defesapokemon1 = pokemons[i].defesa;
+               ataquepokemon1 = pokemons[i].ataque;
+            }
+            // Encontrando o pokemon2 para poder pegar os valores dos poderes
+            if ( strcmp(pokemon2, pokemons[i].nome) == 0) {
+               defesapokemon2 = pokemons[i].defesa;
+               ataquepokemon2 = pokemons[i].ataque;
+            }
+         }
+         // RESULTADO
+      }
+      // LISTA
+      else if ( strcmp(verfstatusf, "list ele") == 0 ) {
+         for ( i = 0 ; i < quantPokemon ; i++) {
+            if (pokemons[i].tipo == "ele") {
+               printf("Pokemon %d: %s", i+1, pokemons[i].nome);
+               printf(" ataque: %d", pokemons[i].ataque);
+               printf(" defesa: %d", pokemons[i].defesa);
+            }
+         }
+      }
+      else if ( strcmp(verfstatusf, "list fog") == 0 ) {
+         for ( i = 0 ; i < quantPokemon ; i++) {
+            if (pokemons[i].tipo == "fog") {
+               printf("Pokemon %d: %s", i+1, pokemons[i].nome);
+               printf(" ataque: %d", pokemons[i].ataque);
+               printf(" defesa: %d", pokemons[i].defesa);
+            }
+         }
+      }
+      
+      scanf("%d", &verif);
+   }
 
-   printf("");
+   if (ataquepokemon1 - defesapokemon2 > ataquepokemon2 - defesapokemon1) {
+      printf("%s deitou %s na porrada\n", pokemon1, pokemon2);
+   } else if ( ataquepokemon2 - defesapokemon1 > ataquepokemon1 - defesapokemon2 ) {
+      printf("%s deitou %s na porrada\n", pokemon2, pokemon1);
+   } else {
+      printf("parece que temos um empate\n");
+   }
 
    return 0;
 }
