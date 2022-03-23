8d3e58ff13c7f16f70bc64ea63f9c9e609f254ba
Robson Thiago <rtth141200@gmail.com>
2022-03-23 17:37:01 -0300

    Manuseando bytes-bits em vetores

---
+++ 01-Aulas-Array-Vetores/Vetores/Matrizes/exer1-13.10.2021-1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+
+
+   int vet[] = {0, 2, 4, 6, 8, 10};
+   int i;
+   // Mostrando o tamanho do vetor em bytes
+   printf("Tamanho em bytes= %ld\n", sizeof(vet));
+   /**
+    * Vai pegar 24 / 4
+    * 24 -> é o tamanho do vetor em bytes/a quantidade que usa
+    * 4 --> é a quantidade de bytes que uma variavel int usa
+    * 6 --> quantidade de elementos que tem o vetor
+    **/
+   // Mostrando a quantidade de elementos no vetor
+   printf("quantidade de elementos= %ld\n", sizeof(vet)/sizeof(int));
+
+
+   /** Fazendo uma outra representação
+    * O sizeof retorna como long int
+    * como eu vou querer a quantidade de elementos e não o tamanho que todo ele ocupa, o int é suficiente
+    */
+   int tam = sizeof(vet) / sizeof(int);
+   // Mostrando a quantidade de elementos no vetor
+   printf("quantidade de elementos= %d\n", tam);
+
+   /** Podemos demonstrar todos os elementos que estão dentro do vetor sem ter declarado o tamanho do vetor
+    * vamos basear o limite do for na variável que recebe a quantidade de elementos do vet.
+    */
+   for( i = 0 ; i < tam ; i++) {
+      printf("vet[%d]= %d\n", i, vet[i]);
+   }
+
+   return 0;
+}
