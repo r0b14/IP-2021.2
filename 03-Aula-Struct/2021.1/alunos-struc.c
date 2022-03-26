a621d5b6a9d67c146de7b83e8bb952b929d605bd
Robson Thiago <rtth141200@gmail.com>
2022-03-26 10:59:08 -0300

    .

---
+++ 03-Aula-Struct/2021.1/alunos-struc.c
@@ -0,0 +1,43 @@
+/* Media de Alunos */
+// Resumiremos o código alunos, agora usando a estrutura para poder melhorar
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 50
+#define T_TURMA 3
+/**
+ * Usaremos uma lista de alunos com nomes e notas.
+ * Verificaremos se os alunos que estão na lista estão iguais ou acima da média
+**/
+
+typedef struct {
+   char nome[SIZE];
+   float nota;
+} Pessoa;
+
+int main () {
+
+   int i;
+   char nomes[T_TURMA][SIZE]; // TENHO 50 NOME DE 70 CARACTERES -> vetores bidimensionais
+   float notas[T_TURMA], soma = 0, mediaglobal = 0; // TENHO 70 NOTAS INDIVIDUAIS
+
+   // Leitura da Turma
+   for ( i = 0 ; i < T_TURMA ; i++ ) {
+      printf("Digite o nome do %d aluno: ", i+1);
+      scanf(" %49[^\n]", nomes[i]);
+      printf("Digite a nota do %d aluno: ", i+1);
+      scanf("%d", &notas[i]);
+      soma += notas[i];
+   }
+
+   mediaglobal = soma / T_TURMA;
+
+   //Selecionando alunos
+   for ( i = 0 ; i < T_TURMA ; i++ ) { 
+      if (notas[i] >= mediaglobal) {
+         printf(" Aluno: %s\n", nomes[i]);
+      }
+   }
+
+   return 0;
+}
