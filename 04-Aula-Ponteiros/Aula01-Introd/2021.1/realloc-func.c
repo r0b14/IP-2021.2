1239cf6f14b236d03ca6641efb0ae5cb986852b6
Robson Thiago <rtth141200@gmail.com>
2022-04-01 14:42:30 -0300

    aplicado os conceitos

--- 04-Aula-Ponteiros/Aula01-Introd/2021.1/realloc-func.c
+++ 04-Aula-Ponteiros/Aula01-Introd/2021.1/realloc-func.c
@@ -1,32 +1,35 @@
 /* Manipulando vetores */
 #include <stdio.h>
 #include <stdlib.h>
+
    /**
-    * #### Funçaõ malloc ####
-    *      Memory Allocato
-    * A função vai pedir a S.O memórias baseado na tipo que foi pedido.
-    * malloc ( n * sizeof(int));
-    * S.O me dê n blocos do tamanho de um inteiro
-    */
+    * #### Função ralloc ####
+    * Aloca para mais ou para menos espaço alocado que desejar
+    * A função vai pedir ao S.O ou reduzir memórias baseado na tipo que foi pedido.
+    * realloc (vet, n * sizeof(int)); -> nesse caso o ponteiro vet vai receber o valor ou 
+    * realloc (n, sizeof(int)); -> nesse caso o n vai multiplicar pelo valor
+    * S.O me dê n blocos do tamanho de um inteiro ou reduza n blocos do tamanho de um inteiro
+   **/
+
 int main() {
 
    int n, i;
    printf("Digite N: ");
    scanf("%d", &n);
-   int *vet;
-   // o valor vai depender de quanto o usuário vai entrar
-   // n = 4; 4 * 4 = 16 bytes alocados
-   vet = (int *) malloc(n * sizeof(int));
+   int *vetAnt = NULL, *vetAt;
+
+   // Realocando para 2x o tamanho do vetor
+   vetAt = (int *) realloc(vetAnt, n * 2 * sizeof(int));
 
    // O teste previne acontecer uma invasao de memoria
-   if(vet == NULL) {
+   if(vetAt == NULL) {
       printf("Error");
       exit(1);
    } else {
-      for (i = 0; i < n; i++) {
-         scanf("%d", &vet[i]);
+      for (i = 0; i < n * 2 * sizeof(int); i++) {
+         scanf("%d", &vetAt[i]);
       }
    }
-   return 0;
 
+   return 0;
 }
