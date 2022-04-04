246ac76188b692492501880e71b03991956b5c6f
Robson Thiago <rtth141200@gmail.com>
2022-04-04 14:00:02 -0300

    melhorada a descrição do programa

--- 04-Ponteiros/Aula2-2021.1/pont-1.c
+++ 04-Ponteiros/Aula2-2021.1/pont-1.c
@@ -1,6 +1,12 @@
+/* Vetor que cresce a depender da necessidade do usuário*/
 #include <stdio.h>
 #include <stdlib.h>
-
+   /**
+    * Um programa que vai executar o vetor a depender da necessidade do usuario.
+    * O vetor não terá uma estrutura fixa, logo, precisamos usar alocação dinâmica.
+    * como a tendeência é que o vetor cresça, não precisaremos usar funções relacionado a desalocação
+    * a função que mais se encaixa a situação atual é a realloc(vet, tam-alocado)
+   **/ 
 int main() {
 
    int n = 0, tmp;
@@ -21,7 +27,11 @@ int main() {
       printf("Digite um valor (<= 0 para): ");
       scanf("%d", &tmp);
    }
-   
+
+   // Apresentando os valores que o usuario inseriu
+   for(int i = 0; i < n; i++) {
+      printf("vet[%d] = %d\n", i+1, vet[i]);
+   }
 
    return 0;
 }
