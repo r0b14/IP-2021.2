37eb3d801b6b8681a8d621825b82773a6bef8c11
Robson Thiago <rtth141200@gmail.com>
2022-04-04 13:46:45 -0300

    vetor crescente com entrada do usuario

--- 04-Ponteiros/Aula2-2021.1/pont-1.c
+++ 04-Ponteiros/Aula2-2021.1/pont-1.c
@@ -3,7 +3,25 @@
 
 int main() {
 
-   int n = 50;
+   int n = 0, tmp;
+   int *vet = NULL;
+   // Ler inteiros do teclado enquanto > 0
+   printf("Digite um valor (<= 0 para): ");
+   scanf("%d", &tmp);
+
+   while ( tmp > 0 ) {
+      vet = (int *) realloc(vet, (n+1) * sizeof(int)); // Alocação Dinamica
+      if (vet == NULL) { // Verificando a alocação
+         printf("Erro Alocação!!\n");
+         exit(1); // O compilador demonstra o S.O que foi algum erro no programa.
+      }
+      vet[n] = tmp; // Inserindo no local de n o valor tmp
+      n++;
+      // Recebendo novamente o valor do usuário e inserindo na posicao n
+      printf("Digite um valor (<= 0 para): ");
+      scanf("%d", &tmp);
+   }
+   
 
    return 0;
 }
