e69c24dd15c32c11c06028804b85ff19dd8a2959
Robson Thiago <rtth141200@gmail.com>
2022-04-13 00:11:19 -0300

    programa em elaboração

--- 04-Ponteiros/03-pont-matriz-struct/familia2.c
+++ 04-Ponteiros/03-pont-matriz-struct/familia2.c
@@ -7,19 +7,49 @@
 typedef struct {
    char nome[29];
    int idade;
+   int *datanascimento;
    float altura;
 } Pessoa;
 
+
+
 int main() {
 
-   Pessoa *familia;
-   int verif, tam, inserido = 0;
-   printf("Digite 0 para poder sair do programa");
-   while ( scanf ("%d", &verif) != 0 ) {
-      inserido += 1;
-      familia = (Pessoa *) malloc(1 * sizeof(Pessoa));
+   Pessoa *familia = NULL;
+   int verif = 1, tam, inserido = 0;
+   while ( verif != 4 ) {
+      //MENU
+      printf("\n\n======= MENU ========\n\n");
+      printf("1 - Inserir familiar\n");
+      printf("2 - Excluir familiar\n");
+      printf("3 - Mostrar familiares\n");
+      printf("4 - Sair de Familiares\n");
+      scanf ("%d", &verif);
+      // Verificando o operador
+      switch ( verif ) {
+         case 1:
+            /**
+            fami
+            printf("Digite 0 para poder sair do programa");
+            inserido += 1;
+            familia = (Pessoa *) realloc( ,1 * sizeof(Pessoa));
+            **/
+         break;
+         case 2:
+            // Excluir familiar
+         break;
+         case 3:
+            // Mostrar Familiares
+         break;
+         case 4:
+            // Sair de familiares
+         break;
+         default:
+            system("pause");
+            system("cls");
    }
 
 
+   system("pause");
    return 0;
 }
