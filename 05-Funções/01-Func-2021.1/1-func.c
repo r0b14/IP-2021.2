69290118923adc48703eb375c7c65b0fb8e9f500
Robson Thiago <rtth141200@gmail.com>
2022-04-14 13:03:55 -0300

    feito os ajustes de funcoes

--- 05-Funções/01-Func-2021.1/1-func.c
+++ 05-Funções/01-Func-2021.1/1-func.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-#define TAM 20
-
-preenchevet( float vet[TAM] ) {
-   float vet[TAM];
+/**
+ * Funções podem ou não retornarem valores. Isso pode influenciar diretamente na declaração da mesma.
+ * Void -> quando ela não retorna diretamente um valor.
+ * 
+ */
+#define TAM 3
+// Vetor é meramente formal -> ele está aqui para poder deixar coerente a forma que a funcao se organiza
+void preenchevet( float vetor[TAM] )
+{
    for(int i = 0; i < TAM; i++)
    {
-      printf("Digite o elemento vet[%d]= ", i+1);
-      scanf("%f", &vet[i]);
+      printf("Digite o elemento vetor[%d]= ", i+1);
+      scanf("%f", &vetor[i]);
+   }
+}
+float somaVet(float vetor[TAM])
+{
+   float soma;
+   for(int i = 0; i < 20; i++)
+   {
+      soma += vetor[i];
    }
 }
 
-int main()
-{
+int main() {
+   /**
+    * A função permite que eu transporte a variavel do main para um local um bloco de processamento e retorne o valor modificado.
+    * Exemplo -> preenchevet vai transportar vet para ser processado na funcao
+    * vet = parametro real (main) => o que vai ser usado de fato
+    * float vet[TAM] = parametro formal (na funcao)
+    */
    float vet[TAM], soma = 0;
    //1. Intuito/Proposito -> Preencher vetor com os dados do usuario
    preenchevet(vet);
-   for(int i = 0; i < 20; i++)
-   {
-      printf("Digite o elemento vet[%d]= ", i+1);
-      scanf("%f", &vet[i]);
-   }
-   
    // 1. Intuito/Proposito -> somar Elementos do Vetor
-   
-   for(int i = 0; i < 20; i++)
-   {
-      soma += vet[i];
-   }
-   /**
-    * 1. Intuito/Proposito -> Apresentar media aritmetica dos dados contidos no vetor
-   **/
-   printf("Media = %.1f", soma/20);
-
+   soma = somaVet(vet);
+   // 1. Intuito/Proposito -> Apresentar media aritmetica dos dados contidos no vetor
+   printf("Media = %.2f", soma/TAM);
    return 0;
 }
