f68a8695a71e6d6cb309592dcec34fcdc2db8cee
Robson-ubnt <56169778+r0b14@users.noreply.github.com>
2022-04-26 10:11:51 -0300

    ajustado arquivo. agora rodando

--- Provas-Anteriores/Praticando1.c
+++ Provas-Anteriores/Praticando1.c
@@ -20,8 +20,9 @@ void cadastrarPessoas(Pessoa **ptr, int *qtd) {
       scanf(" %29[^\n]", aux.nome);
       printf("Digite o preço: ");
       scanf("%lf", &aux.preco);
-      prinf("Digite a quantidade: ");
-      scanf("%d", aux.qt);
+      printf("Digite a quantidade: ");
+      scanf("%d", &aux.qt);
+
       //Ralizando a alocação
       (*ptr) = (Pessoa *) realloc((*ptr), (*qtd + 1) * sizeof(Pessoa));
       if((*ptr) == NULL) {
@@ -30,13 +31,26 @@ void cadastrarPessoas(Pessoa **ptr, int *qtd) {
       }
 
       (*ptr)[*qtd] = aux;
+      (*qtd)++;
    }
-   
+
 }
 
 int main() {
    Pessoa *ptrPessoa = NULL;
+   int qtd = 0;
 
+   cadastrarPessoas(&ptrPessoa, &qtd);
+   printf("\nNo de pessoas: %d\n", qtd);
+   for(int i = 0; i < qtd; i++)
+   {
+      printf("Codigo: %d\n",ptrPessoa[i].codigo);
+      printf("Nome: %s\n", ptrPessoa[i].nome);
+      printf("Preço: %.3lf\n", ptrPessoa[i].preco);
+      printf("Quantidade: %d\n", ptrPessoa[i].qt);
+   }
+
+   free(ptrPessoa);
 
    return 0;
 }
