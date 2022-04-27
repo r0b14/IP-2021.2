e4e0530f1ebd122ebbf55acf77329e1fa3133716
Robson Thiago <rtth141200@gmail.com>
2022-04-27 12:08:33 -0300

    ajustados

--- Prova/rtds.c
+++ Prova/rtds.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define SIZE 100
+#define SIZENAME 20
 /**
  * 𝜀=0, letter=1, number=2, any=9, caractere específico=código ASCII
  * 
@@ -23,7 +24,6 @@ Transicao *carregaAutomato(char *nomeArqBin, long *numEstados) {
 }
 
 void gravaStatus(FILE *arqSaida, char *entrada, char status) {
-
 }
 
 long *localizaEstado(Transicao *automato, long numEstados, int estadoAtual, int *numEstadosAtuais, char qual) {
@@ -40,29 +40,58 @@ void reconhece(FILE *arqSaida, Transicao *automato, long numEstados, char *entra
 
 int main() {
 
-   FILE *arqBin = NULL; // Arquivo criado
-   char txtUsario[SIZE];
+   FILE *arqBin = NULL; // Arquivo binario
+   FILE *arqTxt = NULL; // Arquivo txt
+   Transicao txtInserido;
+   char txtUsario[SIZE], *pontTxtUsario = NULL, nomeArquivo[SIZENAME];
    long numEstados = 0; // Numero total de estados dentro do arquivos
    int condicao, i = 0;
 
-   while (condicao != 0) {
-      // Recebendo a string do usuario
-      printf("Digite a %do string:\n", i+1);
-      scanf(" %(SIZE-1)[^\n]", txtUsario);
-   }
+   // Recebendo o nome do arquivo bin baseado na escolha do usuario
+   printf("Digite o nome do arq binario: ");
+   scanf(" %(SIZENAME-1)[^\n]", nomeArquivo);
 
-   //Criando arquivo
-   arqBin = fopen("ArquivoBinario.bin", "wb");
+   // Recebendo o nome do arquivo bin baseado na escolha do usuario
+   arqBin = fopen(strcat(nomeArquivo,".bin"), "wb");
    if(arqBin == NULL){
       printf("Erro ao abrir o Arquivo");
       fclose(arqBin);
       exit(-1);
    }
-   fwrite(lista, sizeof(int), *numQtd, arqAux);
-   fclose(arqAux);
 
-   carregaAutomato(, );
+   arqTxt = fopen(strcat(nomeArquivo,".txt"), "wb");
+   if (arqTxt == NULL) {
+      printf("Erro ao abrir o Arquivo");
+      fclose(arqTxt);
+      exit(-1);
+   }
 
+   while (condicao != 0) {
+      // Recebendo a string do usuario
+      printf("Digite a %do string:\n", i+1);
+      scanf(" %(SIZE-1)[^\n]", txtUsario);
+      pontTxtUsario = (char *) realloc(pontTxtUsario, (i+1) * sizeof(char));
+      if (pontTxtUsario == NULL) {
+         printf("Error for alocacion\n");
+         exit(-1);
+      }
+      (*pontTxtUsario)[i] = txtUsario;
+      i++; // incrementando o valor de i
+
+      //Verificando o a string inserida pelo usuario
+
+      //Enviando para o arquivo txt o resultado da string inserida
+      gravaStatus(arqTxt, &txtUsario, );
+
+      carregaAutomato();
+      localizaEstado();
+      avaliaTransicao();
+      reconhece();
+   }
+
+
+   //Fechando os arquivos
+   fclose(arqTxt);
    fclose(arqBin);
    return 0;
 }
--- Provas-Anteriores/Praticando1.c
+++ Provas-Anteriores/Praticando1.c
@@ -19,7 +19,7 @@ void cadastrarPessoas(Pessoa **ptr, int *qtd) {
    {
       printf("Digite o nome: ");
       scanf(" %29[^\n]", aux.nome);
-      printf("Digite o preço: ");                                                                                                                          
+      printf("Digite o preço: ");
       scanf("%lf", &aux.preco);
       printf("Digite a quantidade: ");
       scanf("%d", &aux.qt);
